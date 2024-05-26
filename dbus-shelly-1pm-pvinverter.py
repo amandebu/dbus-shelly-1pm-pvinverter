9cf29ece93f5981a49b4ef24e46c53fe090278d1
amandebu <75584382+amandebu@users.noreply.github.com>
2024-05-26 18:38:39 +0200

    Update dbus-shelly-1pm-pvinverter.py
    Make _update crash on exception so that service is restarted on failure

--- dbus-shelly-1pm-pvinverter.py
+++ dbus-shelly-1pm-pvinverter.py
@@ -188,7 +188,7 @@ class DbusShelly1pmService:
        self._lastUpdate = time.time()
     except Exception as e:
        logging.critical('Error at %s', '_update', exc_info=e)
-
+       raise
     # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
     return True
 
